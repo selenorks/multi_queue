3418de8b508e7155e0269b7b127a85bff0d64bd6
Evgeny Fimochkin <fimochkin@gmail.com>
2020-09-29 22:26:20 +0700

    qupdate traivs

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
-git clone -b v2 --depth 1 https://github.com/google/benchmark.git
+git clone  -b v1.5.2 --depth 1 https://github.com/google/benchmark.git
 git clone -b release-1.10.0 --depth 1  https://github.com/google/googletest.git benchmark/googletest
